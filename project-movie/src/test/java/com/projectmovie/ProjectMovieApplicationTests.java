7325ba0d29fe26063453d0ffac5cb68cd31eb7f6
Saroj Khadka <sarojkaka@gmail.com>
2023-02-09 13:20:18 -0500

    start

---
+++ project-movie/src/test/java/com/projectmovie/ProjectMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
